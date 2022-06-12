a6b24d8f4ac6e1ba1150de54d26e3f5b2054f22d
KeyurMistry <keyurmist@gmail.com>
2022-06-12 15:27:46 +0100

    added prompt questions to fill markdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generateMarkdown = ({ title, description, installation, usage, license, contribute, test, email, github}) =>
-`#${title}
+const generateMarkdown = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contribute,
+  test,
+  email,
+  github,
+}) =>
+  `#${title}
 
 ## Table of Contents
 - [Description](#Description)
@@ -53,6 +63,61 @@ ${test}
 You can e-mail me at:${email}
 
 Visit my GitHub ![here](https://github.com/${github})
-`
+`;
 
-const p
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please enter the title of your project",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter a short description of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please give instructions on how to install the application",
+    },
+    {
+      type: "input",
+      name: "usgae",
+      message: "Please give instructions on how to use the application",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Please select a license for your project",
+      choices: [
+        "Apache",
+        "MIT",
+        "GNU:General Public License",
+        "BSD 2-Clause",
+        "BSD 3-Clause",
+      ],
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "Please give details on how to contribute to the project",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Please give details on how to test the project",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter you e-mail address",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your GitHub username",
+    },
+  ]);
+};
